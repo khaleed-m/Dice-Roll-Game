2c277bb217ed45812c8a7e7a4bc43a11b525d9ad
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-12 23:00:11 +0530

    Load and display random dice images using PIL and ImageTk

--- Dice_Roll.py
+++ Dice_Roll.py
@@ -8,4 +8,14 @@ window.geometry("300x400")
 window.configure(bg="lightblue")    
 window.resizable(False, False)
 
+dice =["inverted-dice-1.png", "inverted-dice-2.png", "inverted-dice-3.png",
+        "inverted-dice-4.png", "inverted-dice-5.png", "inverted-dice-6.png"]
+
+image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+image2 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+
+#image_label = tk.Label(window, image=image1, bg="lightblue")
+
+# Load images for dice faces
+dice_images = [ImageTk.PhotoImage(Image.open(f"dice_{i}.png")) for i in range(1, 7)]
 window.mainloop()
